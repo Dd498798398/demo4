19e912d2ca2881172c2164f46009d01c5d8d2a6c
Dd498798398 <52090969+Dd498798398@users.noreply.github.com>
2022-05-03 17:25:12 +0800

    Update Student.java

--- src/main/java/com/example/demo/vo/Student.java
+++ src/main/java/com/example/demo/vo/Student.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class Student {
 
     public Student() {
-        System.out.println("Student()");
+        System.out.println("Student(1)");
     }
 
     private User user;
