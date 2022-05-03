a902d83363a358fe5f7ac413a6ba2a45acc943ad
GlobalName <GlobalEmail>
2022-05-03 18:08:52 +0800

    这是  my——dev 的修改

--- src/main/java/com/example/demo/vo/C.java
+++ src/main/java/com/example/demo/vo/C.java
@@ -7,5 +7,6 @@ package com.example.demo.vo;
 public class C {
     public static void main(String[] args) {
         System.out.println("2. win");
+        System.out.println("这是  my——dev 的修改");
     }
 }
