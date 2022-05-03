7897d93119fa4896656c8529be9011d0ac806fc9
GlobalName <GlobalEmail>
2022-05-03 14:10:18 +0800

    提交 TestGit.java

---
+++ src/main/java/com/example/demo/vo/TestGit.java
@@ -0,0 +1,11 @@
+package com.example.demo.vo;
+
+/**
+ * @author zhangxudong@chunyu.me
+ * @date 2022/5/3 2:07 下午
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+    }
+}
