10ac4ec2bcf342c1809b1d62a823043cba747cac
Dd498798398 <52090969+Dd498798398@users.noreply.github.com>
2022-05-03 17:24:49 +0800

    Update C.java

--- src/main/java/com/example/demo/vo/C.java
+++ src/main/java/com/example/demo/vo/C.java
@@ -6,6 +6,6 @@ package com.example.demo.vo;
  */
 public class C {
     public static void main(String[] args) {
-        System.out.println("1. win");
+        System.out.println("2. win");
     }
 }
